b113ebd8356aa31608a0788a5097a6aead38c228
erich <eriche864@gmail.com>
2021-01-19 20:31:23 -0500

    implement findbyrecipeidandid ingred service impl test

---
+++ src/test/java/guru/springframework/services/IngredientServiceImplTest.java
@@ -0,0 +1,58 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.IngredientCommand;
+import guru.springframework.converters.IngredientToIngredientCommand;
+import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import guru.springframework.domain.Ingredient;
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class IngredientServiceImplTest {
+
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    IngredientService ingredientService;
+
+    public IngredientServiceImplTest() {
+        ingredientToIngredientCommand = new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand());
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        ingredientService = new IngredientServiceImpl(ingredientToIngredientCommand, recipeRepository);
+    }
+
+    @Test
+    public void findByRecipeIdAndId() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(3L);
+
+        recipe.addIngredient(ingredient);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        IngredientCommand ingredientCommand = ingredientService.findByRecipeIdAndId(1L, 3L);
+
+        assertEquals(Long.valueOf(3L), ingredientCommand.getId());
+        assertEquals(Long.valueOf(1L), ingredientCommand.getRecipeId());
+        verify(recipeRepository).findById(anyLong());
+    }
+}
